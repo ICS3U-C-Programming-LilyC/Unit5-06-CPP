233a8ab9d54c56e5a89b4773cffdb8c922bb38f2
Lily Carroll <lily.carroll@stu.ocsb.ca>
2023-12-06 23:52:05 +0000

    Fixing linter errors.

---
+++ rounding.cpp
@@ -0,0 +1,12 @@
+// Copyright (c) 2023 Lily Carroll All rights reserved.
+//
+// Created by: Lily Carroll
+// Created on: Dec/6/2023
+// This program gets a decimal from the user
+// along with how many decimal places they
+// would like to round to and rounds their 
+// number accordingly.
+#include<iostream>
+#include<cmath>
+
+
